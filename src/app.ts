d27cc8f5585f316fdf13dd39238859667d3ff98d
Gabriel Wiggert <gabrielwiggert@gmail.com>
2022-10-10 18:22:12 -0300

    CORS settings

--- src/app.ts
+++ src/app.ts
@@ -11,4 +11,11 @@ app.use(cors());
 app.use(router);
 app.use(errorHandlerMiddleware);
 
+//CORS settings
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+  
 export default app;
